c562fcf7e944a5280d83c7184843b82df9d72596
oleyva93 <oleyva930424@gmail.com>
2023-05-18 00:54:47 -0600

    Fix app component

--- src/app.jsx
+++ src/app.jsx
@@ -4,14 +4,12 @@ import "./index.css";
 
 function App() {
   return (
-    <>
-      <div
-        className="background-pattern flex justify-center items-center h-screen p-20 pattern-cross pattern-red-500 pattern-bg-white 
+    <div
+      className="background-pattern flex justify-center items-center h-screen p-20 pattern-cross pattern-red-500 pattern-bg-white 
   pattern-size-6 pattern-opacity-10"
-      >
-        <ReactTerminalHistory data={fakeData} title="Logs" />
-      </div>
-    </>
+    >
+      <ReactTerminalHistory data={fakeData} title="Logs" />
+    </div>
   );
 }
 
